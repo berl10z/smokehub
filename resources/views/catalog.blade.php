2cbede623822585b3fd3315df9f839883d8297fc
berl10z <dev.berlioz@gmail.com>
2025-03-09 20:19:38 +0600

    fix catalog

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -40,34 +40,35 @@
         </div>
         <button class="mt-3 h-max self-center primary-button" type="submit">Подтвердить</button>
     </form>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        @forelse ($products as $product)
-            <div class="bg-white p-4 rounded-lg shadow-md product" data-id="2" data-name="Товар 2" data-price="2000">
-                <div class="flex justify-center items-center">
-                    <div class="w-64 h-64 sm:w-72 sm:h-72 md:w-80 md:h-80 lg:w-96 lg:h-96 xl:w-64 xl:h-64 overflow-hidden">
-                        <img src="{{ $product->image ? asset('storage/' . $product->image) : 'https://placehold.co/250' }}" alt="{{ $product->name }}" class="w-full h-full object-contain">
+    <div class="mt-5">
+        <div class="divide-y divide-gray-300">
+            @forelse ($products as $product)
+                <div class="py-5 m-5">
+                    <div class="flex items-center">
+                        <div class="w-16 h-16 bg-gray-200 rounded-md flex items-center justify-center">
+                            <img src="{{ $product->image ? asset('storage/' . $product->image) : 'https://placehold.co/64' }}"
+                                 alt="{{ $product->name }}"
+                                 class="w-full h-full object-cover rounded-md">
+                        </div>
+                        <div class="flex-1 ml-4">
+                            <p class="text-gray-600">Категория: {{ $product->category->name ?? 'Без категории' }}</p>
+                            <p class="text-gray-600">Бренд: {{ $product->brand->name ?? 'Без бренда' }}</p>
+                            <p class="text-lg font-semibold text-gray-800">{{ $product->name }}</p>
+                        </div>
+                    </div>
+                    <div class="flex items-center justify-between mt-2">
+                        <p class="text-2xl font-bold">{{ ($product->price) }} ₽ </p>
+                        <form action="{{ route('product.show', $product->id) }}" method="get">
+                            <button class="w-10 h-10 bg-blue-600 hover:bg-blue-700 text-white text-lg font-bold rounded-lg flex items-center justify-center">
+                                +
+                            </button>
+                        </form>
                     </div>
                 </div>
-                <p class="text-lg font-bold">{{ $product->name }}</p>
-                <p class="text-gray-600">Категория: {{ $product->category->name ?? 'Без категории' }}</p>
-                <p class="text-gray-600">Цена: {{ $product->price }}</p>
-                <p class="text-gray-600">Бренд: {{ $product->brand->name ?? 'Без бренда' }}</p>
-                <p class="text-gray-600">
-                    Количество товара:
-                    @if ($product->count > 0)
-                        {{ $product->count }}
-                    @else
-                        нет в наличии
-                    @endif
-                </p>
-                <form action="{{ route('product.show', $product->id) }}" method="get">
-                    <button class="mt-3 primary-button">Подробнее
-                    </button>
-                </form>
-            </div>
-        @empty
-        <p class="text-gray-600 text-xl" >Нет в наличии</p>
-        @endforelse
+            @empty
+                <p class="text-gray-600 text-xl">Нет в наличии</p>
+            @endforelse
+        </div>
     </div>
 </div>
 @endsection
